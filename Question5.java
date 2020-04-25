931a6b4a2cd179b2c95cc573e66bdf42351a6acc
BenChang22 <64120724+BenChang22@users.noreply.github.com>
2020-04-25 19:27:52 +0800

    completed qn 5

--- Question5.java
+++ Question5.java
@@ -26,7 +26,35 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
+  Scanner in = new Scanner(System.in);
+  System.out.print("Enter number of values to calculate : ");
+  int num = in.nextInt();
+  int numlist[] = new int[num];
+  for (int i = 0; i < num; i++){
+     System.out.print("Enter values : ");
+    numlist[i] = in.nextInt();
+  }
+  in.close();
+
+   int mode = 0; 
+    int total = 1 ; 
+
+    for(int i = 0; i < numlist.length; i++)
+    {
+      int count = 0; 
+      for(int j = 0; j < numlist.length; j++)
+      {
+        if(numlist[j] == numlist[i])
+        {
+          count += 1;
+        }
+      }
+      if(count > total)
+      {
+        mode = numlist[i]; 
+      }
+    }
+    System.out.println(mode);
     
   }
 }
