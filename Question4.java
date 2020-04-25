9020d48d6bf583c6a27183075133f6a4f0d4de01
BenChang22 <64120724+BenChang22@users.noreply.github.com>
2020-04-25 17:07:26 +0800

    finished quetion 4

--- Question4.java
+++ Question4.java
@@ -17,7 +17,19 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    Scanner in = new Scanner(System.in);
+     Scanner in = new Scanner(System.in);
+    int num;
+    System.out.println("Input an integer for the base of the triangle");
+    num = in.nextInt();
+       for (int i = num; i > 0; i--){
+      System.out.println(" ");
+       for(int k = i;k > 0; k--){
+       System.out.print("*");
+        }
+        System.out.println(" ");
+       }
+       in.close();
+    }
     
   }
-}
+
